261787b578f2c9a045fdcb0da7b0a2ac8b7b5563
kochetov-dmitrij <dimak.o@mail.ru>
2019-04-16 04:39:01 +0300

    Select category when searching Added 'category' to item description While modifying items, placeholders show the current value

--- imports/ui/containers/ItemDetailView.js
+++ imports/ui/containers/ItemDetailView.js
@@ -53,6 +53,10 @@ class ViewItem extends React.Component {
               <strong>Key Words: </strong>
               {item.keywords}
             </p>
+            <p>
+                <strong>Category: </strong>
+                {item.category}
+            </p>
 
             {item.OwnerID === this.props.currentUser._id ?
               <div>
